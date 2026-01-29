47f35148e60fa1c48cf7ed5758e7b11f97de4dbe
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-29 16:02:01 +0100

    set-up rotte iniziali

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import Product from "./pages/Product";
@@ -6,9 +6,13 @@ import Product from "./pages/Product";
 function App() {
   return (
     <>
-      <HomePage />;
-      <AboutUs />;
-      <Product />;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/prodotti" element={<Product />} />
+          <Route path="/chisiamo" element={<AboutUs />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
