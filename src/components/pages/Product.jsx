499794290d8003645a12f75769f7a1c018fda9af
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-29 20:01:30 +0100

    import axios, useState e usEffect

--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -1,24 +1,11 @@
 // import NavBar from "../NavBar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Product() {
-  return (
-    <>
-      {/* <NavBar /> */}
-      <h2>Product contenuto:</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit
-        consectetur fuga maxime sint incidunt dolore asperiores quos quod? Et
-        dolore repellendus laborum esse eius saepe assumenda, provident
-        explicabo sed facere. Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Sit consectetur fuga maxime sint incidunt dolore
-        asperiores quos quod? Et dolore repellendus laborum esse eius saepe
-        assumenda, provident explicabo sed facere. Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Sit consectetur fuga maxime sint incidunt
-        dolore asperiores quos quod? Et dolore repellendus laborum esse eius
-        saepe assumenda, provident explicabo sed facere.
-      </p>
-    </>
-  );
+  const [products, setProducts] = useState([]);
+
+  return <>{/* <NavBar /> */}</>;
 }
 
 export default Product;
