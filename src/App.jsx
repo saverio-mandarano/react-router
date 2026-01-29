da8b44a84c9b15387691a9badddde5c63ba31139
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-29 15:54:52 +0100

    set-up pagine di base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import Product from "./pages/Product";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <>
+      <HomePage />;
+      <AboutUs />;
+      <Product />;
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h2>HomePages</h2>;
+}
+
+export default HomePage;
