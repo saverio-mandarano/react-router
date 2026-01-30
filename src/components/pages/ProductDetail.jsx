82465dd714fda8c9e249fc7fd95858a5f4153342
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-30 17:27:16 +0100

    completo chiamata al dettaglio salvando i dati in var di stato

--- src/components/pages/ProductDetail.jsx
+++ src/components/pages/ProductDetail.jsx
@@ -1,22 +1,29 @@
-//utilizzo l'hook useParams per estrarre il parametro id dall'url
+// utilizzo l'hook useParams per accedere alle informazioni provenienti dalla rotta dinamica mappata
 import { useParams } from "react-router-dom";
 //utilizzo hook useEffect per gestione chiamata
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const endpointBase = "https://fakestoreapi.com/products/";
 
 export default function ProductDetail() {
-  //recupero valore parametro dinamico grazie ad hook useParams
+  //estraggo valore parametro dinamico definito nella rotta grazie all'hook useParams
   const { id } = useParams();
 
-  //utilizzo il parametro per la chiamata
-  useEffect(() => {
+  //var di stato per salvare informazioni su dettaglio prodotto
+  const [productInfo, setProductInfo] = useState({});
+
+  //funzione che fa chiamata Ajax per fetch dati usando l'id
+  function fetchProductInfo() {
     axios
       .get(`${endpointBase}${id}`)
-      .then((res) => console.log(res.data))
+      .then((res) => setProductInfo(res.data))
       .catch((err) => console.log(`errore nella richiesta end.API`))
       .finally(() => console.log(`end call`));
+  }
+
+  useEffect(() => {
+    fetchProductInfo();
   }, []);
 
   return (
