6ab2e4192837ff17e173732e79b9854d1d2e0b5b
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-31 16:31:28 +0100

    Bonus
    Aggiunto navigate che riporti alla pagina di listato se viene cercato un prodotto che non esiste

--- src/components/pages/ProductDetail.jsx
+++ src/components/pages/ProductDetail.jsx
@@ -1,5 +1,5 @@
 // utilizzo l'hook useParams per accedere alle informazioni provenienti dalla rotta dinamica mappata
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 //utilizzo hook useEffect per gestione chiamata
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -14,18 +14,29 @@ export default function ProductDetail() {
   //var di stato per salvare informazioni su dettaglio prodotto
   const [productInfo, setProductInfo] = useState({});
 
+  //eseguo useNavigate per aver un elemento navigate da utilizzare
+  const navigate = useNavigate();
+
   //funzione che fa chiamata Ajax per fetch dati usando l'id
   function fetchProductInfo() {
     axios
       .get(`${endpointBase}${id}`)
-      .then((res) => setProductInfo(res.data))
-      .catch((err) => console.log(`errore nella richiesta end.API`))
+      .then((res) => {
+        //L'API mi restituisce oggetto vuoto per ID inesistente,
+        //in tal caso lancio errore per interrompere il then e far entrare nel catch
+        if (!res.data.id) throw new Error();
+        setProductInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(`errore nella richiesta end.API`, err);
+        navigate("/prodotti"); //Se c'è errore, o il prodotto non esiste (throw sopra), reindirizzamento alla pagina listato
+      })
       .finally(() => console.log(`end call`));
   }
 
   useEffect(() => {
     fetchProductInfo();
-  }, []);
+  }, [id]);
 
   return (
     <>
